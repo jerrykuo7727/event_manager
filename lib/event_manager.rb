b27aad62409e48f1364d065716267667dcabe84a
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-07-08 01:37:14 +0800

    Parsing with CSV

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,9 @@
-puts "EventManager Initialized!"
+require "csv"
+puts "EventManager initialized."
+
+contents = CSV.open("../event_attendees.csv", headers: true, header_converters: :symbol)
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
+end
