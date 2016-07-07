668e20b1a28cfc5d333c0455cb07ddfb49e575e3
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-07-08 02:40:59 +0800

    Use ERB to form letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -8,20 +9,15 @@ def clean_zipcode(zipcode)
 end
 
 def legislators_by_zipcode(zipcode)
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-
-  legislator_names.join(", ")
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
 puts "EventManager initialized."
 
 contents = CSV.open('../event_attendees.csv', headers: true, header_converters: :symbol)
 
-template_letter = File.read("../form_letter.html")
+template_letter = File.read("../form_letter.erb")
+erb_template = ERB.new(template_letter)
 
 contents.each do |row|
   name = row[:first_name]
@@ -30,8 +26,6 @@ contents.each do |row|
   
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
-
-  puts personal_letter
+  form_letter = erb_template.result(binding)
+  puts form_letter
 end
