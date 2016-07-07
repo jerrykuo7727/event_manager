eaf31b4f488df216d3a96701c595c9a3abb78578
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-07-08 02:09:59 +0800

    Use Sunlight gem to display legislators

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,7 @@
-require "csv"
+require 'csv'
+require 'sunlight/congress'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
@@ -12,6 +15,14 @@ contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
+  
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode}"
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
+  end
+
+  legislators_string = legislator_names.join(", ")
+
+  puts "#{name} #{zipcode} #{legislators_string}"
 end
