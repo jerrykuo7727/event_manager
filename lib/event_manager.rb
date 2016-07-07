8756fb5b340b64e7c12822d664527acf0366a3ed
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-07-08 02:13:11 +0800

    Move displaying legislators to a method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -7,6 +7,16 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
+  end
+
+  legislator_names.join(", ")
+end
+
 puts "EventManager initialized."
 
 contents = CSV.open("../event_attendees.csv", headers: true, header_converters: :symbol)
@@ -16,13 +26,7 @@ contents.each do |row|
 
   zipcode = clean_zipcode(row[:zipcode])
   
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  legislators = legislators_by_zipcode(zipcode)
 
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-
-  legislators_string = legislator_names.join(", ")
-
-  puts "#{name} #{zipcode} #{legislators_string}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
