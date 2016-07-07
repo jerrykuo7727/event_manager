6663d357fb4386423809c87672c3d933a179adb2
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-07-08 02:22:00 +0800

    Add html template of form letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -19,7 +19,7 @@ end
 
 puts "EventManager initialized."
 
-contents = CSV.open("../event_attendees.csv", headers: true, header_converters: :symbol)
+contents = CSV.open('../event_attendees.csv', headers: true, header_converters: :symbol)
 
 contents.each do |row|
   name = row[:first_name]
