2ecfdf0b4303b8db7b4e43967b5d2044f9ea1806
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-07-08 01:44:14 +0800

    format the zipcode display

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,21 @@
 require "csv"
+
 puts "EventManager initialized."
 
 contents = CSV.open("../event_attendees.csv", headers: true, header_converters: :symbol)
+
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than 5 digits, truncate it to the first 5 digits
+  # if the zip code is less than 5 digits, add zeros to the front until it becomes five digits
+  if zipcode.length < 5
+    zipcode = zipcode.rjust(5, "0")
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  end
+
   puts "#{name} #{zipcode}"
 end
