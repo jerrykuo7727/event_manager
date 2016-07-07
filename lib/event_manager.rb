3e40534f6c9cd31b3e84a8d961c5a1115d64b474
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-07-08 02:26:13 +0800

    Form letters with template letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -21,6 +21,8 @@ puts "EventManager initialized."
 
 contents = CSV.open('../event_attendees.csv', headers: true, header_converters: :symbol)
 
+template_letter = File.read("../form_letter.html")
+
 contents.each do |row|
   name = row[:first_name]
 
@@ -28,5 +30,8 @@ contents.each do |row|
   
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  personal_letter = template_letter.gsub('FIRST_NAME', name)
+  personal_letter.gsub!('LEGISLATORS', legislators)
+
+  puts personal_letter
 end
